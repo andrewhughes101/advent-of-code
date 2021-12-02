3e0678fc84a57885d44ced5c635947b5c30d6ad1
Andrew Hughes <Andrew.Hughes1@ibm.com>
2021-12-02 14:53:19 +0000

    Add day one and two

---
+++ 2021/Day1.java
@@ -0,0 +1,57 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day1 {
+    private static Integer previousWindow;
+    private static Integer increases;
+
+    public static void main(String[] args) throws IOException {
+        increases = 0;
+        List<String> depthList = Files.readAllLines(Paths.get("Day1Input.txt"));
+        List<Integer> summedDepths = new ArrayList<Integer>();
+        for(int i = 0; i < (depthList.size() - 2); i++) {
+            summedDepths.add(Integer.parseInt(depthList.get(i)) + Integer.parseInt(depthList.get(i+1)) + Integer.parseInt(depthList.get(i+2)));
+        }
+		for(Integer currentWindow : summedDepths) {
+            if(previousWindow != null) {
+                if(currentWindow > previousWindow) {
+                    increases++;
+                }
+            }
+            previousWindow = currentWindow;
+        }
+        System.out.println("Total increases: " + increases);
+    }
+}
+
+// Part 1
+/*
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Day1 {
+    private static Integer previousDepth;
+    private static Integer increases;
+
+    public static void main(String[] args) throws IOException {
+        increases = 0;
+        List<String> depthList = Files.readAllLines(Paths.get("Day1Input.txt"));
+        Stream<String>depthStream = depthList.stream();
+		depthStream.mapToInt(depth -> Integer.parseInt(depth)).forEach(depth -> {
+            if(previousDepth != null) {
+                if(depth > previousDepth) {
+                    increases++;
+                }
+            }
+            previousDepth = depth;
+        });
+        System.out.println("Total increases: " + increases);
+    }
+}
+*/
---
+++ 2021/Day2.java
@@ -0,0 +1,61 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Day2 {
+    public static void main(String[] args) throws IOException {
+        List<String> commandList = Files.readAllLines(Paths.get("Day2Input.txt"));
+        Integer x = 0;
+        Integer y = 0;
+        Integer aim = 0;
+        for(String command : commandList) {
+            Integer amount = Character.getNumericValue(command.charAt((command.length()-1)));
+            if(command.charAt(0) == 'f') {
+                x+=amount;
+                y+=(aim*amount);
+            } else if(command.charAt(0) == 'd') {
+                aim+=amount;
+            } else if(command.charAt(0) == 'u') {
+                aim-=amount;
+            }
+        }
+        System.out.println("X mulitplied by Y: " + (x*y));
+    }
+}
+
+//forward -> adds to x (also y increased by aim*)
+//down -> adds to aim
+//up -> removes from aim
+
+//Part 1
+/*
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Day2 {
+    public static void main(String[] args) throws IOException {
+        List<String> commandList = Files.readAllLines(Paths.get("Day2Input.txt"));
+        Integer x = 0;  //horizontal
+        Integer y = 0;  //vertical/depth
+        for(String command : commandList) {
+            Integer amount = Character.getNumericValue(command.charAt((command.length()-1)));
+            if(command.charAt(0) == 'f') {
+                x+=amount;
+            } else if(command.charAt(0) == 'd') {
+                y+=amount;
+            } else if(command.charAt(0) == 'u') {
+                y-=amount;
+            }
+        }
+        System.out.println("X mulitplied by Y: " + (x*y));
+    }
+}
+
+//forward -> adds to x
+//down -> adds to y
+//up -> removes from y
+
+*/
