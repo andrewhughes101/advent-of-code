a228a147d8b423505c6624715d916e3f529d2b61
Andrew Hughes <Andrew.Hughes1@ibm.com>
2021-12-03 18:00:50 +0000

    format everything

--- 2021/Day1.java
+++ 2021/Day1.java
@@ -12,12 +12,13 @@ public class Day1 {
         increases = 0;
         List<String> depthList = Files.readAllLines(Paths.get("Day1Input.txt"));
         List<Integer> summedDepths = new ArrayList<Integer>();
-        for(int i = 0; i < (depthList.size() - 2); i++) {
-            summedDepths.add(Integer.parseInt(depthList.get(i)) + Integer.parseInt(depthList.get(i+1)) + Integer.parseInt(depthList.get(i+2)));
+        for (int i = 0; i < (depthList.size() - 2); i++) {
+            summedDepths.add(Integer.parseInt(depthList.get(i)) + Integer.parseInt(depthList.get(i + 1))
+                    + Integer.parseInt(depthList.get(i + 2)));
         }
-		for(Integer currentWindow : summedDepths) {
-            if(previousWindow != null) {
-                if(currentWindow > previousWindow) {
+        for (Integer currentWindow : summedDepths) {
+            if (previousWindow != null) {
+                if (currentWindow > previousWindow) {
                     increases++;
                 }
             }
@@ -29,29 +30,29 @@ public class Day1 {
 
 // Part 1
 /*
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.stream.Stream;
-
-public class Day1 {
-    private static Integer previousDepth;
-    private static Integer increases;
-
-    public static void main(String[] args) throws IOException {
-        increases = 0;
-        List<String> depthList = Files.readAllLines(Paths.get("Day1Input.txt"));
-        Stream<String>depthStream = depthList.stream();
-		depthStream.mapToInt(depth -> Integer.parseInt(depth)).forEach(depth -> {
-            if(previousDepth != null) {
-                if(depth > previousDepth) {
-                    increases++;
-                }
-            }
-            previousDepth = depth;
-        });
-        System.out.println("Total increases: " + increases);
-    }
-}
-*/
+ * import java.io.IOException;
+ * import java.nio.file.Files;
+ * import java.nio.file.Paths;
+ * import java.util.List;
+ * import java.util.stream.Stream;
+ * 
+ * public class Day1 {
+ * private static Integer previousDepth;
+ * private static Integer increases;
+ * 
+ * public static void main(String[] args) throws IOException {
+ * increases = 0;
+ * List<String> depthList = Files.readAllLines(Paths.get("Day1Input.txt"));
+ * Stream<String>depthStream = depthList.stream();
+ * depthStream.mapToInt(depth -> Integer.parseInt(depth)).forEach(depth -> {
+ * if(previousDepth != null) {
+ * if(depth > previousDepth) {
+ * increases++;
+ * }
+ * }
+ * previousDepth = depth;
+ * });
+ * System.out.println("Total increases: " + increases);
+ * }
+ * }
+ */
--- 2021/Day3.java
+++ 2021/Day3.java
@@ -13,42 +13,42 @@ public class Day3 {
         oxygenSet.addAll(diagnosticList);
         List<String> co2Set = new ArrayList<String>();
         co2Set.addAll(diagnosticList);
-        
+
         List<String> onesSet = new ArrayList<String>();
         List<String> zerosSet = new ArrayList<String>();
-        
-        for(int i=0; i<diagnosticList.get(0).length(); i++) {
-            for(String binaryStr : oxygenSet) {
-                if(Character.getNumericValue(binaryStr.charAt(i)) == 1) {
+
+        for (int i = 0; i < diagnosticList.get(0).length(); i++) {
+            for (String binaryStr : oxygenSet) {
+                if (Character.getNumericValue(binaryStr.charAt(i)) == 1) {
                     onesSet.add(binaryStr);
                 } else {
                     zerosSet.add(binaryStr);
                 }
             }
-            if(oxygenSet.size() > 1) {
-                if(onesSet.size() - zerosSet.size() > -1) {
-                    //1s most common
+            if (oxygenSet.size() > 1) {
+                if (onesSet.size() - zerosSet.size() > -1) {
+                    // 1s most common
                     oxygenSet.removeAll(new HashSet<String>(zerosSet));
                 } else {
-                    //0s most common
+                    // 0s most common
                     oxygenSet.removeAll(new HashSet<String>(onesSet));
                 }
             }
             onesSet.clear();
             zerosSet.clear();
-            for(String binaryStr : co2Set) {
-                if(Character.getNumericValue(binaryStr.charAt(i)) == 1) {
+            for (String binaryStr : co2Set) {
+                if (Character.getNumericValue(binaryStr.charAt(i)) == 1) {
                     onesSet.add(binaryStr);
                 } else {
                     zerosSet.add(binaryStr);
                 }
             }
-            if(co2Set.size() > 1) {
-                if(onesSet.size() - zerosSet.size() > -1) {
-                    //1s most common
+            if (co2Set.size() > 1) {
+                if (onesSet.size() - zerosSet.size() > -1) {
+                    // 1s most common
                     co2Set.removeAll(new HashSet<String>(onesSet));
                 } else {
-                    //0s most common
+                    // 0s most common
                     co2Set.removeAll(new HashSet<String>(zerosSet));
                 }
             }
@@ -59,49 +59,50 @@ public class Day3 {
         String oxygenRate = oxygenSet.get(0);
         String co2Rate = co2Set.get(0);
 
-        Integer oxygen = Integer.parseInt(oxygenRate,2);
-        Integer co2 = Integer.parseInt(co2Rate,2);
-        System.out.println("Life support rating: " + (oxygen*co2));
+        Integer oxygen = Integer.parseInt(oxygenRate, 2);
+        Integer co2 = Integer.parseInt(co2Rate, 2);
+        System.out.println("Life support rating: " + (oxygen * co2));
     }
 }
 
-//Part 1
+// Part 1
 /*
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
-
-public class Day3 {
-    public static void main(String[] args) throws IOException {
-        List<String> diagnosticList = Files.readAllLines(Paths.get("Day3Input.txt"));
-        //go through each bit, if 0 do -1 from counter, if 1 do +1, total neg more 0s, total pos more 1s
-        String gammaRate = "";
-        Integer counter = 0;
-        for(int i=0; i<diagnosticList.get(0).length(); i++) {
-            for(String binaryStr : diagnosticList) {
-                if(Character.getNumericValue(binaryStr.charAt(i)) == 1) {
-                    counter++;
-                } else {
-                    counter--;
-                }
-            }
-            if(counter > 0) {
-                gammaRate+=1;
-            } else {
-                gammaRate+=0;
-            }
-            counter = 0;
-        }
-
-        String epsilonRate = gammaRate;
-        epsilonRate = epsilonRate.replaceAll("0", "x");
-        epsilonRate = epsilonRate.replaceAll("1", "0");
-        epsilonRate = epsilonRate.replaceAll("x", "1");
-
-        Integer gam = Integer.parseInt(gammaRate,2);
-        Integer eps = Integer.parseInt(epsilonRate,2);
-        System.out.println("result: " + (eps*gam));
-    }
-}
-*/
+ * import java.io.IOException;
+ * import java.nio.file.Files;
+ * import java.nio.file.Paths;
+ * import java.util.List;
+ * 
+ * public class Day3 {
+ * public static void main(String[] args) throws IOException {
+ * List<String> diagnosticList = Files.readAllLines(Paths.get("Day3Input.txt"));
+ * //go through each bit, if 0 do -1 from counter, if 1 do +1, total neg more
+ * 0s, total pos more 1s
+ * String gammaRate = "";
+ * Integer counter = 0;
+ * for(int i=0; i<diagnosticList.get(0).length(); i++) {
+ * for(String binaryStr : diagnosticList) {
+ * if(Character.getNumericValue(binaryStr.charAt(i)) == 1) {
+ * counter++;
+ * } else {
+ * counter--;
+ * }
+ * }
+ * if(counter > 0) {
+ * gammaRate+=1;
+ * } else {
+ * gammaRate+=0;
+ * }
+ * counter = 0;
+ * }
+ * 
+ * String epsilonRate = gammaRate;
+ * epsilonRate = epsilonRate.replaceAll("0", "x");
+ * epsilonRate = epsilonRate.replaceAll("1", "0");
+ * epsilonRate = epsilonRate.replaceAll("x", "1");
+ * 
+ * Integer gam = Integer.parseInt(gammaRate,2);
+ * Integer eps = Integer.parseInt(epsilonRate,2);
+ * System.out.println("result: " + (eps*gam));
+ * }
+ * }
+ */
